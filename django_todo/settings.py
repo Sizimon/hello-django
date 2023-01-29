599dec97975c8ab817b51e61c5cdd3ac73f4aa68
Sizimon <siizmonsus@gmail.com>
2023-01-29 21:33:17 +0000

    Prepared to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -90,9 +90,6 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
-DATABASES['default'] = dj_database_url.config()
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
