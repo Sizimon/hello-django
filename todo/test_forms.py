bb4b292aef3f10af861a41ec0de7c886e1eac9d4
Sizimon <siizmonsus@gmail.com>
2023-01-29 21:25:53 +0000

    Setup heroku and link to elephant

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -8,7 +8,7 @@ class TestItemForm(TestCase):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
-        self.assertEqual(form.errors['name'][0], 'This field is required.')  
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
         form = ItemForm({'name': 'Test Todo Item'})
--- todo/test_models.py
+++ todo/test_models.py
@@ -1,1 +1,13 @@
 from django.test import TestCase
+from .models import Item
+
+
+class TestViews(TestCase):
+
+    def test_items_default_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
