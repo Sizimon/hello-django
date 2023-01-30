f530ef7886dcc5e380e703dd8e60280505f27ec6
Sizimon <siizmonsus@gmail.com>
2023-01-30 16:37:24 +0000

    cont

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['shhmon-django-todo-app.herokuapp.com']
