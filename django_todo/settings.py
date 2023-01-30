99a70489533e41c7c84a5913f054f97b9c0d6822
Sizimon <siizmonsus@gmail.com>
2023-01-30 16:57:37 +0000

    heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shhmon-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['shhmon-django-todo-app.herokuapp.com', 'localhost']
 
 
 # Application definition
